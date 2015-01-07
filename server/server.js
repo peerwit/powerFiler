f7334d06b0305b0cc4ea64b21058510c02ac9f29
Hack Reactor Students <accounts@hackreactor.com>
2015-01-06 16:35:37 -0800

    Hello

--- server/server.js
+++ server/server.js
@@ -13,11 +13,27 @@ app.get('/', function(req, res) {
   res.sendfile(path.resolve(__dirname + '/../client/index.html'));
 });
 app.get('/downloads/:id', function(req, res) {
-  res.sendfile(path.resolve(__dirname + '/../client/index.html'));
+  express.static(path.resolve(__dirname + '/../client/' + req.param('id')));
 });
 
-app.post('/uploads/:id' , function(req,res){
+app.use(multer({dest: './uploads/:id', rename: function(fieldname, filename) {
+      console.log("Hellosfd ");
+      console.log(this.originalname, filename);
+    }
+  })
+)
+
+// app.post('/uploads/:id', [multer({ dest: './uploads/'}), function(req, res){
+//     console.log("Hello");
+//     var id = req.param('id');
+//     console.log(req.body) // form fields
+//     console.log(req.files) // form files
+//     res.status(204).end()
+// }]);
+
+app.get('/uploads/' , function(req,res){
   var id = req.param('id');
-})
+  res.end("Hello asf")
+});
 
 app.listen(3000);
